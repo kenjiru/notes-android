5e8298d0b23976ffee5e30374058c6a7dd08802e
kenjiru <kenjiru.ro@gmail.com>
2014-10-13 00:41:28 +0200

    Handle saving and restoring the activity instance state.

--- app/src/main/java/ro/kenjiru/notes/ui/list/NotesListActivity.java
+++ app/src/main/java/ro/kenjiru/notes/ui/list/NotesListActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Parcelable;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,6 +16,8 @@ import android.widget.ListView;
 
 import com.activeandroid.query.Select;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import ro.kenjiru.notes.R;
@@ -24,17 +27,77 @@ import ro.kenjiru.notes.ui.SettingsActivity;
 
 public class NotesListActivity extends ListActivity  {
     private static final int RESULT_SETTINGS = 1;
+    private static final String LIST_INSTANCE_STATE = "LIST_INSTANCE_STATE";
+    private static final String LIST_ADAPTER_ARRAY = "LIST_ADAPTER_ARRAY";
+
+    private boolean stateRestored = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
-
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
-        setAdapter();
-        setClickListener();
-        setScrollListener();
+        restoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        saveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        restoreInstanceState(savedInstanceState);
+    }
+
+    private void saveInstanceState(Bundle outState) {
+        ListView listView = getListView();
+        NotesAdapter adapter = (NotesAdapter) listView.getAdapter();
+
+        outState.putParcelable(LIST_INSTANCE_STATE, listView.onSaveInstanceState());
+        outState.putSerializable(LIST_ADAPTER_ARRAY, (ArrayList<Note>) adapter.getAllItems());
+    }
+
+    private void restoreInstanceState(Bundle savedInstanceState) {
+        // onCreate and onSaveInstanceState might both execute
+        if (!stateRestored) {
+            createListAdapter(savedInstanceState);
+            restoreListInstanceState(savedInstanceState);
+
+            setClickListener();
+            setScrollListener();
+
+            stateRestored = true;
+        }
+    }
+
+    private void createListAdapter(Bundle savedInstanceState) {
+        ArrayList<Note> notes = new ArrayList<Note>();
+
+        if (savedInstanceState != null) {
+            Serializable listAdapterArray = savedInstanceState.getSerializable(LIST_ADAPTER_ARRAY);
+
+            if (listAdapterArray != null) {
+                notes = (ArrayList<Note>) listAdapterArray;
+            }
+        }
+
+        ListAdapter listAdapter = new NotesAdapter(this, notes);
+        setListAdapter(listAdapter);
+    }
+
+    private void restoreListInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            Parcelable listInstanceState = savedInstanceState.getParcelable(LIST_INSTANCE_STATE);
+            if (listInstanceState != null) {
+                getListView().onRestoreInstanceState(listInstanceState);
+            }
+        }
     }
 
     private void setClickListener() {
@@ -52,11 +115,6 @@ public class NotesListActivity extends ListActivity  {
 
     }
 
-    private void setAdapter() {
-        ListAdapter listAdapter = new NotesAdapter(this);
-        setListAdapter(listAdapter);
-    }
-
     private void setScrollListener() {
         getListView().setOnScrollListener(new EndlessScrollListener() {
             @Override
@@ -71,7 +129,7 @@ public class NotesListActivity extends ListActivity  {
                         // Do something after 5s = 5000ms
                         loadMoreData(page);
                     }
-                }, 500);
+                }, 1000);
             }
         });
     }
@@ -80,8 +138,8 @@ public class NotesListActivity extends ListActivity  {
         List<Note> notes = new Select()
                 .from(Note.class)
                 .orderBy("title ASC")
-                .offset((page-2)*3)
-                .limit(3)
+                .offset(page*3)
+                .limit(7)
                 .execute();
 
         NotesAdapter listAdapter = (NotesAdapter) getListAdapter();
