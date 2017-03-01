c4f9420d23169adbc4bec85b1fbdae3f5bad5268
kenjiru <kenjiru.ro@gmail.com>
2017-03-01 23:55:51 +0100

    Make a Dropbox call from within the sync service.

--- app/src/main/java/ro/kenjiru/notes/dropbox/DropboxSyncService.java
+++ app/src/main/java/ro/kenjiru/notes/dropbox/DropboxSyncService.java
@@ -4,7 +4,10 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.dropbox.core.v2.users.FullAccount;
 
 public class DropboxSyncService extends IntentService {
     private final IBinder mBinder = new LocalBinder();
@@ -21,6 +24,20 @@ public class DropboxSyncService extends IntentService {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "DropboxSyncService starting", Toast.LENGTH_SHORT).show();
+
+        new GetCurrentAccountTask(DropboxClientFactory.getClient(), new GetCurrentAccountTask.Callback() {
+            @Override
+            public void onComplete(FullAccount result) {
+                String displayName = result.getName().getDisplayName();
+
+                Toast.makeText(DropboxSyncService.this, "Dropbox user: " + displayName, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e(getClass().getName(), "Failed to get account details.", e);
+            }
+        }).execute();
 
         return super.onStartCommand(intent, flags, startId);
     }
